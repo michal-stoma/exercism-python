7bb202d0e50860533fcceb0963c4e686a843fe4f
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-01-12 21:31:29 +0100

    Flatten Array: second iteration.
    Generator yields all values now, checking for None values moved to flatten().

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -7,10 +7,10 @@ def _flatten_generator(nested_list):
         if isinstance(item, (list, tuple)) and not isinstance(item, str):
             # firts convert tuple to list
             nested_list = list(item) + nested_list
-        elif item is not None:
+        else:
             yield item
 
 
 def flatten(iterable):
     """Flatten nested list."""
-    return [i for i in _flatten_generator(iterable)]
+    return [i for i in _flatten_generator(iterable) if i is not None]
