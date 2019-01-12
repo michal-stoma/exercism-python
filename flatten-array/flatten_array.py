185f69315220c73a527ac1229db314a21d8b1ee9
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-01-12 21:12:28 +0100

    Flatten Array: first iteration.

---
+++ flatten-array/flatten_array.py
@@ -0,0 +1,16 @@
+def _flatten_generator(nested_list):
+    while nested_list:
+        item = nested_list.pop(0)
+
+        # Explicitly for test cases. Generic solution could check if
+        # item is iterable and not str.
+        if isinstance(item, (list, tuple)) and not isinstance(item, str):
+            # firts convert tuple to list
+            nested_list = list(item) + nested_list
+        elif item is not None:
+            yield item
+
+
+def flatten(iterable):
+    """Flatten nested list."""
+    return [i for i in _flatten_generator(iterable)]
