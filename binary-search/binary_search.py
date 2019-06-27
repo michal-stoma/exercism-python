f668ee3e285ce7633f73f6161a73f71acedf9b48
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-06-27 23:01:03 +0200

    Binary Search: third iteration.

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -7,13 +7,14 @@ def binary_search(list_of_numbers, number):
         raise ValueError('Value not found')
 
     found = None
-    idx = length // 2
 
     # initial boundaries of a list (full list)
     start = 0
     end = length - 1
 
     while not found:
+        idx = (start + end) // 2
+
         if start > end:
             raise ValueError('Value not found')
 
@@ -26,15 +27,8 @@ def binary_search(list_of_numbers, number):
             # left side of array
             end = idx - 1
 
-            # Make sure that idx will move by 1 at least. Using math.ceil()
-            # instead of // could be another way to do that.
-            idx_offset = (end - start) // 2 + 1
-            idx -= idx_offset
-
         else:
             # right side of array
             start = idx + 1
-            idx_offset = (end - start) // 2 + 1
-            idx += idx_offset
 
     return idx
