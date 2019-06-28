2e34c09e9b3c5d5addf26b7449fcb7336b9eb7c2
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-06-29 00:11:31 +0200

    Grains: second solution with bitshift instead of power.

--- grains/grains.py
+++ grains/grains.py
@@ -2,11 +2,13 @@ def square(number):
     if number < 1 or number > 64:
         raise ValueError('Number must be between 1 and 64')
 
-    return 2 ** (number - 1)
+# Worth remembering: bitwise operation are faster than power (2 ** n)
+# but in case of such a small powers it's irrelevant.
+    return 1 << (number - 1)
 
 
 def total(number):
     if number < 1 or number > 64:
         raise ValueError('Number must be between 1 and 64')
 
-    return 2 ** number - 1
+    return (1 << number) - 1
