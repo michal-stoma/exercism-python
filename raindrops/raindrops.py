8c86f73bacb0808699b58b1d0317f23a4b7bcd99
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-07-05 11:20:52 +0200

    Raindrops: second iteration.

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,11 +1,11 @@
 def convert(number):
-    FACTORS = [3, 5, 7]
-    FACTORS_MAP = {
+    FACTORS = {
         3: 'Pling',
         5: 'Plang',
         7: 'Plong'
     }
 
-    sound = [FACTORS_MAP[factor] for factor in FACTORS if number % factor == 0]
+    sound = [FACTORS[factor] for factor in sorted(FACTORS)
+             if number % factor == 0]
 
     return ''.join(sound) or str(number)
