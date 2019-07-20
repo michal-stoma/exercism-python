eca9453da0d2f3fe4095b3dde7e24fbaea38806c
Michal Stoma <25387095+michal-stoma@users.noreply.github.com>
2019-07-21 00:05:36 +0200

    RNA Transcription: exercise's update

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,15 +1,15 @@
-"""Exercism -> rna-transcription."""
+RNA_MAP = {
+    'G': 'C',
+    'T': 'A',
+    'C': 'G',
+    'A': 'U'
+}
 
 
 def to_rna(dna):
-    """Return RNA for provided DNA."""
-    if not all(i in 'GCTA' for i in dna):
-        return ''
-
     rna = []
-    for i in range(len(dna)):
-        if dna[i] == 'G' or dna[i] == 'T':
-            rna.append(dna[i].replace('G', 'C').replace('T', 'A'))
-        else:
-            rna.append(dna[i].replace('C', 'G').replace('A', 'U'))
+
+    for i in dna:
+        rna.append(RNA_MAP[i])
+
     return ''.join(rna)
